947d8408e6c8d8c0a387e67fa6b381b0beca7fcc
Marcos Antonio Martins <mantonioprof@gmail.com>
2020-03-05 20:37:54 -0300

    feat: add maximum width and height to global style

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -2,6 +2,13 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+
+	html, body, #__next {
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+	}
 `;
 
 export default GlobalStyle;
