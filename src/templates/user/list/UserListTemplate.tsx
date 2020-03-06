d8b3096aebe8ecfc8ae00942ae249db75466e5c6
Marcos Antonio Martins <mantonioprof@gmail.com>
2020-03-06 05:31:09 -0300

    feat: add UserListTemplate

---
+++ src/templates/user/list/UserListTemplate.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Table, HeaderCell } from "components/table";
+
+import * as M from "./mock";
+import * as S from "./styled";
+
+export const UserListTemplate = () => {
+  const header: HeaderCell[] = [{ child: "Login" }, { child: "ID" }];
+  const rows = M.firstPage.map(data => ({ columns: Object.values(data) }));
+
+  return (
+    <S.Container>
+      <Table header={header} rows={rows} rowClickHandler={console.log} />
+    </S.Container>
+  );
+};
