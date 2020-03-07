d68b75f40e774bb00f46b36bf7f3ee9b051be9aa
Marcos Antonio Martins <mantonioprof@gmail.com>
2020-03-07 15:19:30 -0300

    feat: add pagination

--- src/templates/user/list/UserListTemplate.tsx
+++ src/templates/user/list/UserListTemplate.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Table, HeaderCell } from 'components/table';
 
@@ -7,12 +7,46 @@ import * as S from './styled';
 
 export const UserListTemplate = () => {
   const header: HeaderCell[] = [{ child: 'Login' }, { child: 'ID' }];
-  const rows = M.firstPage.map(data => ({ columns: Object.values(data) }));
+  const [rows, setRows] = useState(
+    M.firstPage.map(data => ({ columns: Object.values(data) }))
+  );
+
+  const [paginationProps, setPaginationProps] = useState({
+    firstItemIndex: 1,
+    lastItemIndex: 10,
+    previousPageDisabled: true,
+    nextPageDisabled: false,
+    previousPageClickHandler: () => {
+      setPaginationProps({
+        ...paginationProps,
+        firstItemIndex: paginationProps.firstItemIndex -= 10,
+        lastItemIndex: paginationProps.lastItemIndex -= 10,
+        previousPageDisabled: paginationProps.previousPageDisabled = true,
+        nextPageDisabled: paginationProps.nextPageDisabled = false,
+      });
+      setRows(M.firstPage.map(data => ({ columns: Object.values(data) })));
+    },
+    nextPageClickHandler: () => {
+      setPaginationProps({
+        ...paginationProps,
+        firstItemIndex: paginationProps.firstItemIndex += 10,
+        lastItemIndex: paginationProps.lastItemIndex += 10,
+        previousPageDisabled: paginationProps.previousPageDisabled = false,
+        nextPageDisabled: paginationProps.nextPageDisabled = true,
+      });
+      setRows(M.secondPage.map(data => ({ columns: Object.values(data) })));
+    },
+  });
 
   return (
     <S.Container>
       <S.TableContainer>
-        <Table header={header} rows={rows} rowClickHandler={console.log} />
+        <Table
+          header={header}
+          rows={rows}
+          rowClickHandler={console.log}
+          paginationProps={paginationProps}
+        />
       </S.TableContainer>
     </S.Container>
   );
