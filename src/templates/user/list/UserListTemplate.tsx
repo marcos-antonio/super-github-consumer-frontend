5b222bfcef57049837130baec207145af5021726
Marcos Antonio Martins <mantonioprof@gmail.com>
2020-03-07 13:50:58 -0300

    feat: centralize users table

--- src/templates/user/list/UserListTemplate.tsx
+++ src/templates/user/list/UserListTemplate.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import { Table, HeaderCell } from 'components/table'
+import { Table, HeaderCell } from 'components/table';
 
-import * as M from './mock'
-import * as S from './styled'
+import * as M from './mock';
+import * as S from './styled';
 
 export const UserListTemplate = () => {
-  const header: HeaderCell[] = [{ child: 'Login' }, { child: 'ID' }]
-  const rows = M.firstPage.map(data => ({ columns: Object.values(data) }))
+  const header: HeaderCell[] = [{ child: 'Login' }, { child: 'ID' }];
+  const rows = M.firstPage.map(data => ({ columns: Object.values(data) }));
 
   return (
     <S.Container>
-      <Table header={header} rows={rows} rowClickHandler={console.log} />
+      <S.TableContainer>
+        <Table header={header} rows={rows} rowClickHandler={console.log} />
+      </S.TableContainer>
     </S.Container>
-  )
-}
+  );
+};
