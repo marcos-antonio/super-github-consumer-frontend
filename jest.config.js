cb3c4d2fcd2b869d127391b5de7eb57939762caa
Marcos Antonio Martins <mantonioprof@gmail.com>
2020-03-08 17:07:09 -0300

    feat: add jest test configuration

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/index.ts',
+    '!**/styled.ts',
+    '!**/GlobalStyle.ts',
+    '!**/node_modules/**',
+  ],
+  collectCoverage: true,
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/cssTransform.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+};
